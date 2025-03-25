87edfcfa37edeb4e63ef01ebf405af13cdd5e87e
Hungitdev <hungitdev@gmail.com>
2025-03-25 17:56:28 -0600

    made it cleaner,shorter and more effective. An significantly better version

--- hung.js
+++ hung.js
@@ -1,115 +1,102 @@
-
-
+console.log("Hello world");
 let humanScore=0;
     computerScore=0;
-let humanSelection= '';
-    computerSelection="";
-    matchResult="";
     
-function getComputerChoice(max) {
-    const random= Math.floor(Math.random()*max);
-    if(random===0){
-        computerSelection="rock";
-        console.log(" computer:rock");
-    }
-    else if(random===1){
-        computerSelection="rock";
-        console.log("computer:papper");
-    }
-    else{ 
-        computerSelection="scissors";
-        console.log("computer:scissors");  
+
+function getComputerChoice(){
+    x= Math.floor(Math.random()*3);
+    if (x===0){
+        return "rock"
+    } else if(x===1){
+        return "paper"
+    }else {
+        return "scissors"
     }
 }
-getComputerChoice(3);
 
-  
+
 function getHumanChoice(){
-    const userInput=prompt("Rock,papper,scissors?");
-    if(userInput.toLowerCase()==="scissors"){
-        humanSelection="scissors";
-        console.log("Human choice:scissors");
-    } else if(userInput.toLowerCase()==="rock"){
-        humanSelection="rock";
-        console.log("Human choice:rock");
-    } else if(userInput.toLowerCase()==="papper"){
-        humanSelection="papper";
-        console.log("Human choice: papper");
+    userInput=prompt("Rock,paper,scissors?")
+    return userInput.toLowerCase()
+}
+
+
+
+
+function playRound(humanChoice,computerChoice){
+    if (humanChoice===computerChoice) {
+        console.log("H: "+humanChoice);
+        console.log("C: "+computerChoice);
+        console.log("Tie!!!");
+    } else {
+        switch(humanChoice){
+            case "rock":
+                if(computerChoice==="paper"){
+                    console.log("H: "+humanChoice);
+        console.log("C: "+computerChoice);
+                    console.log("You lose");
+                    computerScore++
+                }else if(computerChoice==="scissors"){
+                    console.log("H: "+humanChoice);
+        console.log("C: "+computerChoice);
+                    console.log("You Won!!");
+                    humanScore++
+                }
+                break;
+            case "paper":
+                if(computerChoice==="scissors"){
+                    console.log("H: "+humanChoice);
+        console.log(computerChoice);
+                    console.log("C: "+"You lose");
+                    computerScore++
+                } else if(computerChoice==="rock"){
+                    console.log("H: "+humanChoice);
+        console.log("C: "+computerChoice);
+                    console.log("You won");
+                    humanScore++
+                }
+                break;
+            case "scissors":
+                if(computerChoice==="rock"){
+                    console.log("H: "+humanChoice);
+        console.log("C: "+computerChoice);
+                    console.log("You lose");
+                    computerScore++
+                }else if(computerChoice==="paper"){
+                    console.log("H: "+humanChoice);
+        console.log("C: "+computerChoice);
+                    console.log("You won");
+                    humanScore++
+                }
+                break;
+        }
+
     }
-  
+     
 }
-  
 
-getHumanChoice(); 
-
-function playround(){
-    if (humanSelection==="rock" && computerSelection==="papper"){
-        console.log("You lose");
-        computerScore++;
-    } else if (humanSelection==="rock" && computerSelection==="scissors"){
-        console.log("You win");
-        humanScore++;
-    }else  if (humanSelection==="rock" && computerSelection==="rock"){
-        console.log("tie");
-    }else if (humanSelection==="papper" && computerSelection==="scissors"){
-        console.log("you lose");
-        computerScore++;
-    }else if(humanSelection==="papper"&& computerSelection==="rock"){
-        console.log("you win");
-        humanScore++;
-    }else if( humanSelection==="papper" && computerSelection==="papper"){
-        console.log("tie");
-    } else if(humanSelection==="scissors"&& computerSelection==="papper"){
-        console.log("you win");
-        humanScore++;
-    } else if( humanSelection==="scissors"&& computerSelection==="rock"){
-        console.log("you lose");
-        computerScore++;
-    } else if(humanSelection==="scissors"&& computerSelection==="scissors"){
-        console.log("tie");
-    }
+function PlayGame(){
+    const humanSelection=getHumanChoice();
+    const computerSelection=getComputerChoice();
+playRound(humanSelection,computerSelection);
+console.log("Human score: "+humanScore);
+console.log("Computer score: "+computerScore);
 
 }
-playround();
-console.log("human point:"+humanScore);
-console.log("computer point"+computerScore);
-
-function playGame(){
-    getHumanChoice();
-    getComputerChoice(3);
-    playround();
-    console.log("human point:"+humanScore);
-    console.log("computer point"+computerScore);
-
-    getHumanChoice();
-    getComputerChoice(3);
-    playround();
-    console.log("human point:"+humanScore);
-    console.log("computer point"+computerScore);
-
-    getHumanChoice();
-    getComputerChoice(3);
-    playround();
-    console.log("human point:"+humanScore);
-    console.log("computer point"+computerScore);
-    getHumanChoice();
-    getComputerChoice(3);
-    playround();
-    console.log("human point:"+humanScore);
-    console.log("computer point"+computerScore);
-
-}
-playGame();
+PlayGame();
+PlayGame();
+PlayGame();
+PlayGame();
+PlayGame();
 function result(){
     if(humanScore>computerScore){
-        console.log("you won!!!")
-    }
-    else if(humanScore<computerScore){
-        console.log("You lost!!!")
-    }else {
-        console.log("TIE!!")
+        return "Congratulation!!!You won"
+    }else if(humanScore===computerScore){
+        return "TIEEEEEE!!!!!"
+    } else{
+        return "You loseeeee!!!!!"
     }
 }
-result();
+console.log(result());
 
 
